4744930c2618907f35917d5b45b916693aa99280
jamestklo <tklo@cs.ubc.ca>
2014-12-08 14:16:34 -0800

    updated path to data

--- mixNmatchMF_data/mixNmatchMF_data_IMDB.m
+++ mixNmatchMF_data/mixNmatchMF_data_IMDB.m
@@ -1,5 +1,5 @@
 function [M] = mixNmatchMF_data_IMDB()
-	load '../../MatrixFactorizationData/IMDB.mat';
+	load '../mixNmatchMF_mat/IMDB.mat';
 	M = mixNmatchMF_data_subDim(Problem.A, 25000, 25000, 1, 1);
 	size(M)
 	nnz(M)
