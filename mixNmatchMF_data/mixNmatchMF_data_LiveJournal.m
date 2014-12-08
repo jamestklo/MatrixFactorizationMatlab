490b0162fad872097703d6cb94743f551f782df5
jamestklo <tklo@cs.ubc.ca>
2014-12-08 15:52:41 -0800

    updated data access

---
+++ mixNmatchMF_data/mixNmatchMF_data_LiveJournal.m
@@ -0,0 +1,5 @@
+function [ljournal] = mixNmatchMF_data_digg()
+	load '../mixNmatchMF_mat/subset_ljournal-2008.mat';
+	size(ljournal)
+	nnz(ljournal)
+end
--- mixNmatchMF_data/mixNmatchMF_data_digg.m
+++ mixNmatchMF_data/mixNmatchMF_data_digg.m
@@ -1,6 +1,6 @@
 function [M] = mixNmatchMF_data_digg()
 	load '../mixNmatchMF_mat/digg12month1.mat';
-	M = mixNmatchMF_data_subDim(Problem.A, 25000, 25000, 1, 1);
+	M = mixNmatchMF_data_subDim(Problem.A, 1000, 1000, 1, 1);
 	size(M)
 	nnz(M)
 end
