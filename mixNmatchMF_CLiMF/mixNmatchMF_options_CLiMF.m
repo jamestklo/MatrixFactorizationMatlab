39fcb96fbf5bb75524e27eefa88b914ab5e84f9b
jamestklo <tklo@cs.ubc.ca>
2014-12-07 20:53:45 -0800

    refactored files

--- mixNmatchMF_CLiMF/mixNmatchMF_options_CLiMF.m
+++ mixNmatchMF_CLiMF/mixNmatchMF_options_CLiMF.m
@@ -2,7 +2,7 @@ function [options] = mixNmatchMF_options_CLiMF(options)
 	addpath '../mixNmatchMF_L2/';
     
 	options.objectiveAll = @mixNmatchMF_objective_CLiMF;
-	options.objectiveAt = @mixNmatchMF_lossAt_L2;
+	options.objectiveAt = @mixNmatchMF_objectiveAt_CLiMF;
 
 	options.regularizeAt = @mixNmatchMF_regularizeAt_L2;
 end
